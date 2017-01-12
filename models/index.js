b06613fbf81500ea7c52c521e55f6e335accd7b1
NightCatSama <642163903@qq.com>
2017-01-12 21:36:15 +0800

    update

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ import user from './user'
 
 mongoose.Promise = global.Promise
 
+// mongoose.connect(`mongodb://${config.database.username}:${config.database.password}@${config.db_host}:${config.db_port}/${config.db}?authSource=admin`)
 mongoose.connect(`mongodb://${config.db_host}:${config.db_port}/${config.db}`)
 
 let db = mongoose.connection
