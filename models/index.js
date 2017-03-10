af466933870578b9807005d45c639175936e1665
陈慕杰 <642163903@qq.com>
2017-03-10 17:16:37 +0800

    fix some bug

--- models/index.js
+++ models/index.js
@@ -5,11 +5,8 @@ import user from './user'
 
 mongoose.Promise = global.Promise
 
-let uri = `mongodb://${process.env.NODE_ENV === 'production' ? `${config.database.username}:${config.database.password}` : ''}@${config.db_host}:${config.db_port}/${config.db}`
+let uri = `mongodb://${process.env.NODE_ENV === 'production' ? `${config.database.username}:${config.database.password}@` : ''}${config.db_host}:${config.db_port}/${config.db}`
 
-
-console.log('=======================================')
-console.log(uri)
 mongoose.connect(uri)
 
 let db = mongoose.connection
