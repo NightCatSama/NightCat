46906d5562280fedcf61da6c8f993f39bc571f34
NightCatSama <642163903@qq.com>
2017-07-27 10:13:01 +0800

    fi

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,19 +12,19 @@ export const signinRequire = (req, res, next) => {
 
 /*  未登录跳转到登录页  */
 export const userRequired = (req, res, next) => {
-	if (!req.session || !req.session.token) {
-		return res.status(302)
-			.redirect(`/Sign?message=${encodeURIComponent('请先登录')}&link=${req.path}`)
-	}
+	// if (!req.session || !req.session.token) {
+	// 	return res.status(302)
+	// 		.redirect(`/login?message=${encodeURIComponent('请先登录')}&link=${req.path}`)
+	// }
 	next()
 }
 
 /*  需要管理员权限  */
 export const userAdminRequired = (req, res, next) => {
-	if (!req.session || !req.session.token || !req.session.admin) {
-		return res.status(302)
-			.redirect(`/Sign?message=${encodeURIComponent('没有管理员权限')}&link=${req.path}`)
-	}
+	// if (!req.session || !req.session.token || !req.session.admin) {
+	// 	return res.status(302)
+	// 		.redirect(`/login?message=${encodeURIComponent('没有管理员权限')}&link=${req.path}`)
+	// }
 	next()
 }
 
